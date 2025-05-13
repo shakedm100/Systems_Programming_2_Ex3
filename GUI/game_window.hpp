187dd0ae28e9c763e5aeeeea0498434dabbd2b66
shakedm100 <shaked1mi@gmail.com>
2025-05-13 20:55:52 +0300

    Finished game_window.hpp documentation

--- GUI/game_window.hpp
+++ GUI/game_window.hpp
@@ -1,7 +1,15 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-// Launches the game window for `players` using `font`
-// blocks until that window is closed.
+/**
+ * Launches the main game window
+ * @param players how many players there are in the current game
+ */
 void gameWindow(int players);
+
+/**
+ * This method shows a prompt window that asks for the player's name
+ * @param playerIndex the player's index you want to know his name
+ * @return the player's input
+ */
 std::string promptPlayerName(int playerIndex);
